5f36d3b6932245466cd3cdd27b4560ce3d949bd0
calebchronowski <calebchronowski@gmail.com>
2023-12-07 21:27:59 -0500

    hardcoded functions for f(x), g(x) and their gradients

--- augmented_lagrange.py
+++ augmented_lagrange.py
@@ -7,29 +7,64 @@ tol = 1.e-2 # desired tolerance
 sp_tol = 1.e-3 # subproblem tolerance
 
 
-def fn(x):
+
+# f(x) = e^{3*x1} + e^{-4*x2}
+def f(x):
     x1, x2 = x[0], x[1]
     return np.e**(3*x1) + np.e**(4*x2)
 
 
-grad_fn(x):
+def grad_fn(x):
     x1, x2 = x[0], x[1]
+    output = np.array([
+        [3*np.e**(3*x1)],
+        [-4*np.e**(-4*x2)]
+        ])
+    return output
+
+
+def grad2_fn(x):
+    x1, x2 = x[0], x[1]
+    output = np.array([
+        [9*np.e**(3*x1), 0],
+        [0, 16*np.e**(-4*x2)]
+        ])
+    return output
+
+
+# g(x) = x1^2 + x2^2 -1
+def g(x):
+    x1, x2 = x[0], x[1]
+    return x1 + x2 - 1
+
+
+def grad_g(x):
+    x1, x2 = x[0], x[1]
+    output = np.array([
+        [2*x1],
+        [2*x2]
+        ])
     
-    return
+
+def grad2_g(x):
+    np.array([
+        [2, 0],
+        [0, 2]
+        ])
 
 
-def A_ρ(fn, x, λ, ρ):
+def A_ρ(f, x, λ, ρ):
     '''
     the augmented lagrange function
     '''
 
 
 
-def subproblem(fn, x, λ, ρ):
+def subproblem(f, x, λ, ρ):
     '''
     an unconstrained subproblem computes x_k+1 by minimizing an augmented lagrange formula
     
-    minimize A(x, λ) = fn(x) - λ.T * g(x) + 0.5 * ρ g(x).T * g(x)
+    minimize A(x, λ) = f(x) - λ.T * g(x) + 0.5 * ρ g(x).T * g(x)
     '''
     
 
