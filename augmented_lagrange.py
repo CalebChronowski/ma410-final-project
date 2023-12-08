created augmented lagrange gradient functions, fixed arithmetic errors throughout

augmented_lagrange.py
@@ -3,30 +3,14 @@ import numpy as np
 from logger import Logger
 
 
-# initial values
-x0 = np.array([
-    [1],
-    [-1]
-    ])
-λ0 = -1
-ρ0 = 10
-tol = 1.e-2 # main error tolerance
-sp_tol = 1.e-3 # subproblem error tolerance ###<Can this be dynamic?>###
-
-
-# data
-# this will be accessed to store data for the graphs and other metadata 
-data = Logger
-
-
 # f(x) = e^{3*x1} + e^{-4*x2}
 def f(x):
-    x1, x2 = x[0], x[1] # rewriting the variables this way allows me to make the formula look as close to handwritten math as possible
+    x1, x2 = x[0][0], x[1][0] # rewriting the variables this way allows me to make the formula look as close to handwritten math as possible
     return np.e**(3*x1) + np.e**(4*x2)
 
 
 def grad_f(x):
-    x1, x2 = x[0], x[1]
+    x1, x2 = x[0][0], x[1][0]
     output = np.array([
         [3*np.e**(3*x1)],
         [-4*np.e**(-4*x2)]
@@ -35,7 +19,8 @@ def grad_f(x):
 
 
 def grad2_f(x):
-    x1, x2 = x[0], x[1]
+    x1, x2 = x[0][0], x[1][0]
+
     output = np.array([
         [9*np.e**(3*x1), 0],
         [0, 16*np.e**(-4*x2)]
@@ -47,34 +32,31 @@ def grad2_f(x):
 
 # g(x) = x1^2 + x2^2 -1
 def g(x):
-    x1, x2 = x[0], x[1]
+    x1, x2 = x[0][0], x[1][0]
     return x1 + x2 - 1
 
 
 def grad_g(x):
-    x1, x2 = x[0], x[1]
+    x1, x2 = x[0][0], x[1][0]
     output = np.array([
         [2*x1],
         [2*x2]
         ])
+    return output
     
 
 def grad2_g(x):
-    np.array([
+    # x isn't necessary as an argument, but g(x) is how the math would be handwritten and I wanted to preserve that
+    output = np.array([
         [2, 0],
         [0, 2]
         ])
-
+    return output
 
 
 # Lagrange
-def L(x, λ):
+def L(x, λ): # this is probably useless garbage
     return f(x) - λ*g(x)
-
-
-def grad_L(x, λ):
-    return grad_f(x) - λ*grad_g(x)
-
 
 
 # Augmented Lagrange
@@ -85,6 +67,44 @@ def A(f, x, λ, ρ):
     return f(x) + λ*g(x) + 0.5*ρ*g(x)**2
 
 
+def grad_A(f, x, λ, ρ):
+    # we're not updating rho apparently
+    x1, x2 = x[0][0], x[1][0]
+    pd_wrt_x1 = 20*x1*(x1**2 + x2**2 -1) - 2*λ*x1 + 3*np.e**(3*x1)
+    pd_wrt_x2 = 20*x2*(x1**2 + x2**2 - 1) - 2*λ*x2 - 4*np.log(3)*(1/81)**(x2) ## this is off by a hundredth
+    print(f"pd wrt x1: {pd_wrt_x1}")
+    print(f"pd wrt x2: {pd_wrt_x2}")
+        
+    output = np.array([
+        [pd_wrt_x1],
+        [pd_wrt_x2]
+        ])
+    return output
+
+
+def grad2_A(f, x, λ, ρ):
+    x1, x2 = x[0][0], x[1][0]
+    pd_wrt_x1x1 = 20*(3*x1**2 + x2**2 - 1)- 2*λ + 9*np.e**(3*x1) # top left
+    pd_wrt_x2x2 = 20*(3*x2**2 + x1**2 - 1) - 2*λ + 16*(np.log(3)**2)*(1/81)**(x2) # bottom right
+    pd_wrt_x1x2 = 40*x1*x2 
+    pd_wrt_x2x1 = pd_wrt_x1x2 # same
+    
+    output = np.array([
+        [pd_wrt_x1x1, pd_wrt_x1x2],
+        [pd_wrt_x2x1, pd_wrt_x2x2]
+    ])
+    return output
+
+###############################################################
+# vetigial?
+def grad_L(x, λ, ρ):
+    return grad_f(x) - λ*grad_g(x)
+
+def grad2_L(x, λ, ρ):
+    return grad2_f(x) + λ * grad2_g(x) # verify this
+################################################################
+
+
 
 # The full Augmented Lagrange method
 def subproblem(f, x, λ, ρ):
@@ -93,6 +113,8 @@ def subproblem(f, x, λ, ρ):
     
     minimize A(x, λ) = f(x) - λ.T * g(x) + 0.5 * ρ g(x).T * g(x)
     '''
+    ### x_k+1 = x_k + (-1 * inv(grad2_L(x)) * grad_L(x) ) ?
+    ### thought this was a newton's method problem?
     
 
 
@@ -129,4 +151,18 @@ def augmented_lagrange(fn, x0, λ0, ρ0, tol=1e-2, sp_tol=1e-2):
 
 
 if __name__ == "__main__":
+    # initial values
+    x0 = np.array([
+        [-1],
+        [1]
+        ])
+    λ0 = -1
+    ρ0 = 10
+    tol = 1.e-2 # main error tolerance
+    sp_tol = 1.e-3 # subproblem error tolerance ###<Can this be dynamic?>###
+
+    # data
+    # this will be accessed to store data for the graphs and other metadata 
+    data = Logger
+
     pass